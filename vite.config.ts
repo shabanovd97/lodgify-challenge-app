06b48d798f0cf928530ac7c53548c105523933e4
Dmytro Shabanov <shabanovd97@gmail.com>
2025-01-14 21:42:27 +0100

    add images

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), svgr()],
 });
